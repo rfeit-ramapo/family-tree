4c4d36cc27cff0d8fbcbda37cac02a94d6d5015e
rfeit <rfeit@ramapo.edu>
2024-10-03 18:16:16 -0400

    Initial commit

---
+++ client/src/components/UserCard.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    .user-card-component
+      .user-card-component__avatar
+        img(:src='`${avatar}`' class='user-card-component__avatar-image')
+  
+      .user-card-component__name {{ username }}
+  
+      .user-card-component__email {{ email }}
+  
+      .user-card-component__action
+        button(@click="$emit('contactUser')") Contact {{ lastName }}
+</template>
